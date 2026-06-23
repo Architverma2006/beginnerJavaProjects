d12dfb77cb1efc591ad506f9e9982fa75b634b0d
Archit Verma <158739786+Architverma2006@users.noreply.github.com>
2026-06-24 02:37:10 +0530

    tempConversion.java
    Beginner tempratur conversion programme from °C --> F and vice.I learned how to use ternary operators here.

---
+++ tempConversion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class tempConversion {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to (C/F): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemp =(unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 5/9) + 32;
+
+        System.out.printf("New temperature is %f %s\n", newTemp, unit);
+
+
+
+    }
+}
